db97e870c5e9e4ea63440189ac8a22c467db1fe7
AlexWebArt <elusiv@mail.ru>
2023-04-28 23:31:49 +0600

    comleted ImageManagerFunc 28.04.23

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ImageManager from './components/ImageManagerFunc';
+import './css/main.css';
+
+function App() {
+  return (
+    <div className="main-content">
+      <ImageManager />
+    </div>
+  );
+}
+
+export default App;
